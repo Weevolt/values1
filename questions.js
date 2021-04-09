fe971b1b7d84185a2f433836c8736eb434a4f9c9
Weevolt <82128484+Weevolt@users.noreply.github.com>
2021-04-09 14:35:48 -0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -142,14 +142,5 @@ questions = [
             "govt": 0,
             "scty": 0
         }
-    },
-    {
-        "question": "Question.",
-        "effect": {
-            "econ": 0,
-            "dipl": 0,
-            "govt": 0,
-            "scty": 0
-        }
     }
 ];
